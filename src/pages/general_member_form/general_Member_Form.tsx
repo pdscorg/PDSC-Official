d6180c0d00e8de1452b06e739dc31f6c7631f050
Nabaraj Bhandari <nabarajbhandari2005@gmail.com>
2026-06-25 12:33:17 +0545

    Added: new page for gener al member form /general-member

---
+++ src/pages/general_member_form/general_Member_Form.tsx
@@ -0,0 +1,14 @@
+//Importing Components
+import Navbar from "../../components/Navbar/Navbar";
+import GeneralMembersForm from "../../components/GeneralMembersForm/GeneralMembersForm";
+import Footer from "../../components/Footer/Footer";
+
+export default function GeneralMembersFormPage() {
+  return (
+    <>
+      <Navbar />
+      <GeneralMembersForm />
+      <Footer />
+    </>
+  );
+}
